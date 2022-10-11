f4bffef0f28170f5fed2bd93a904a80543770424
brewchetta <chett.tiller@gmail.com>
2022-10-11 15:52:48 -0400

    setup and ready

---
+++ phase-1/01-debugging/index.js
@@ -0,0 +1,25 @@
+console.log("%c --- Debugging ---", "color: yellow");
+
+// let's do a syntax error
+//  L what is compilation? what is runtime? what is an interpreted language?
+
+if ("I am true" == true {
+  console.log("I am true!")
+}
+
+// let's do a reference error
+//  L can we talk about things out of scope? that haven't been defined yet?
+
+// let's divide by zero
+//  L yeah we can't do that...
+
+// let's do a type error -> call a method on undefined...
+//  L first let's start with an array (of books maybe?)
+//  L let's not find something and then call a method on it
+//  L what is conditional chaining?
+
+// for ALL of these -> what line is the error on? what happens if we can't do something?
+
+// let's talk about return vs console.log...
+
+document.querySelector('h1').textContent = "no more bugs!"
